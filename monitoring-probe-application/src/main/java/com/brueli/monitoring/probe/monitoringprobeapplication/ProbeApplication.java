7a86ceadfd9dd73d57744e63a36259cff79d82bd
Ramon Bruelisauer <ramon.bruelisauer@gmail.com>
2020-07-14 12:04:56 +0200

    added hostname and port to server configuration

--- monitoring-probe-application/src/main/java/com/brueli/monitoring/probe/monitoringprobeapplication/ProbeApplication.java
+++ monitoring-probe-application/src/main/java/com/brueli/monitoring/probe/monitoringprobeapplication/ProbeApplication.java
@@ -5,14 +5,15 @@ import java.util.Arrays;
 import javax.annotation.PreDestroy;
 
 import com.brueli.monitoring.probe.monitoringprobecore.IMonitor;
-import com.brueli.monitoring.probe.monitoringprobesample.SampleMonitorConfiguration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -24,6 +25,7 @@ import io.prometheus.client.exporter.HTTPServer;
 @ComponentScan(basePackages = {
 	"com.brueli.monitoring.probe.monitoringprobesample"
 })
+@EnableConfigurationProperties(ServerConfiguration.class)
 public class ProbeApplication {
 
 	private final Logger logger = LoggerFactory.getLogger(ProbeApplication.class);
@@ -44,31 +46,12 @@ public class ProbeApplication {
 	@Order(2)
 	public ApplicationRunner dumpBeans(ApplicationContext ctx) {
 		return args -> {
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			System.out.println("Configured Beans");
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
 				System.out.println("- " + beanName);
 			}
-		};
-	}
-
-	@Bean
-	@Order(3)
-	public ApplicationRunner dumpConfig(SampleMonitorConfiguration config) {
-		return args -> {
-			System.out.println("configuration properties from application.properties");
-			if (config == null) {
-				System.out.println("config is null");
-				return;
-			}
-			System.out.println("subsystem=" + config.getSubsystem());
-			System.out.println("name=" + config.getName());
-			System.out.println("help=" + config.getHelp());
-			System.out.println("numThreads=" + config.getNumThreads());
-			System.out.println("delay=" + config.getDelay());
-			System.out.println("initialDelay=" + config.getInitialDelay());
-			System.out.println("dontInterruptIfRunning=" + config.getDontInterruptIfRunning());
 		};
 	}
 
@@ -81,14 +64,17 @@ public class ProbeApplication {
 	@Order(0) 
 	public ApplicationRunner runHttpServer(ServerConfiguration config) {
 		return args -> {
-			logger.info("Starting prometheus exporter HTTPServer");
-			Integer port = config.getPort();
 			String hostname = config.getHostname();
-			if (hostname != null) {
-				server = new HTTPServer(hostname, port, true);
-			} else {
+			Integer port = config.getPort();
+
+			logger.info("Starting prometheus exporter HTTPServer");
+			String serverAddress = "http://" + hostname + ":" + port.toString();
+			if (hostname.equals("*")) {
 				server = new HTTPServer(port, true);
+			} else {
+				server = new HTTPServer(hostname, port, true);
 			}
+			logger.info("Serving prometheus metrics under: " + serverAddress);
 		};
 	}
 
--- monitoring-probe-application/src/main/java/com/brueli/monitoring/probe/monitoringprobeapplication/ServerConfiguration.java
+++ monitoring-probe-application/src/main/java/com/brueli/monitoring/probe/monitoringprobeapplication/ServerConfiguration.java
@@ -1,10 +1,14 @@
 package com.brueli.monitoring.probe.monitoringprobeapplication;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "exporter.http.server")
 public class ServerConfiguration {
+    @Value("${port:9100}")
     private int port;
+
+    @Value("${hostname:*}")
     private String hostname;
     
     public Integer getPort() { return port; }
